9658481eca0c913a2208f0eddf5d1824aae0eaaa
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-07-31 01:37:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}"
+introduction("Josh") 
+introduction("Dan")
 end  
 
 def introduction_with_language(name, language)
@@ -9,5 +11,3 @@ end
 def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
-introduction("Josh") 
-introduction("Dan")
