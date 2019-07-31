8844221bf89adecc6ce2c28e10391e9b25a39439
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-07-31 01:41:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,3 +9,8 @@ end
 def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+language = "Ruby"
+def introduction_with_language_optional(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
